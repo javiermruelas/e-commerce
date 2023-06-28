d1521b99a28e9e9d235d04c12b2b12c86fcdfb52
javiermruelas <javiermruelas@gmail.com>
2023-06-28 00:11:21 -0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    // Simple usage
+    '@nuxtjs/eslint-module',
+
+    // With options
+    // ['@nuxtjs/eslint-module', { /* module options */ }]
+  ]
+})
