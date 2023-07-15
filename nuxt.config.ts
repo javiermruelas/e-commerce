9342a40bf37519766659cbf7bc0d69b5d94c6dfa
javiermruelas <javiermruelas@gmail.com>
2023-07-14 19:44:47 -0700

    prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
-    "@nuxtjs/eslint-module"
-  ],
-  imports: {
-    
-  },
+  modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/eslint-module"],
+  imports: {},
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
