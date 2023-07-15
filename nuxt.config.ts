579d11ee5bf3bb237c0e5d254421d0d80eb87f29
javiermruelas <javiermruelas@gmail.com>
2023-07-15 10:00:21 -0700

    added responsive font variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/eslint-module"],
+  modules: ["@vueuse/nuxt", "@pinia/nuxt", "@nuxtjs/eslint-module"],
   imports: {},
   css: ["~/assets/css/main.css"],
   postcss: {
