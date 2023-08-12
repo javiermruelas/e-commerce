848fb0cb121e61664a67e246b275bf535e3549c3
javiermruelas <javiermruelas@gmail.com>
2023-08-12 10:56:37 -0700

    stars are now tailwind yellow-300

--- components/RatingStars.vue
+++ components/RatingStars.vue
@@ -1,16 +1,13 @@
 <template>
-  <div>
+  <div class="mx-auto">
     <div
       v-for="(star, index) in stars"
       :key="index"
       class="inline-block">
-      <div class="rating bg-yellow-300 w-8 h-8">
-        <div class="star w-8 h-8"></div>
-      </div>
+      <div class="star w-8 h-8"></div>
     </div>
     
-    <span class="text-sm">{{ rating.rate }}</span>
-    <span class="text-sm">({{ rating.count }})</span>
+    <p class="text-center"><span class="text-sm">{{ rating.rate }}</span> <span class="text-sm">({{ rating.count }})</span></p>
   </div>
 </template>
 
@@ -19,18 +16,7 @@ const props = defineProps<{
   rating: { rate: number; count: number };
 }>();
 
-const stars = Math.ceil(props.rating.rate);
-
-// function applyLastStarStyling():void {
-//   const lastStarEl: Element = document.getElementsByClassName('rating')[+stars];
-//   const lastStarRatingEl = lastStarEl.childNodes[0] as HTMLElement;
-//   const progress = props.rating.rate % 1.0;
-//   lastStarRatingEl.style.width = `${progress}%`;
-// }
-
-// onMounted(() => {
-//   applyLastStarStyling();
-// });
+const stars = Math.floor(props.rating.rate);
 </script>
 
 <style scoped>
