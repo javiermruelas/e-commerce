0751f6cbe0337cfd18f547708a266047de0a7d11
javiermruelas <javiermruelas@gmail.com>
2023-08-12 17:00:28 -0700

    updated product card styling

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div
     :id="productId"
-    class="shadow-sm bg-neutral-100 p-2 grid grid-cols-2 gap-1"
+    class="shadow-sm bg-neutral-100 p-2 grid grid-cols-2 gap-4 rounded"
   >
     <img :src="image" :alt="description" class="w-max h-max" />
     <div id="productInfo" class="flex flex-col align-center justify-center">
       <h1 class="text-base">{{ title }}</h1>
-      <p class="text-sm">{{ description }}</p>
-      <span class="text-base">{{ price }}</span>
+      <p class="text-sm">{{ shortenedDesc }}</p>
+      <span class="text-base">{{ '$' + price }}</span>
       <RatingStars :rating="rating" />
       <span class="text-xs">{{ category.toUpperCase() }}</span>
     </div>
@@ -25,6 +25,8 @@ const props = defineProps<{
   category: string;
 }>();
 const productId = `product-${props.id}`;
+const descCharLimit = 45;
+const shortenedDesc = props.description.length > 20 ? props.description.slice(0, descCharLimit) + '...' : props.description;
 </script>
 
 <style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,6 @@
 <template>
   <p>Products Page!</p>
-  <div class="container mx-auto px-4 grid">
+  <div class="container mx-auto px-4 grid gap-y-4">
     <ProductCard
       v-for="(product, index) in products"
       :key="index"
